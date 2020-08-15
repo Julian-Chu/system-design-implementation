c27710e89b669113a013811ba6a3ff9f2e35d369
Julian Chu <yulang.chu@gmail.com>
2020-08-15 12:20:47 +0200

    add error handlung

--- golang/urlshort-redis/app.go
+++ golang/urlshort-redis/app.go
@@ -16,8 +16,8 @@ type App struct {
 }
 
 type shortenReq struct {
-	URL                string `json:"url" validate:"nonzero"`
-	ExpirationInMinute int64  `json:"expiration_in_minute" validate:"min=0"`
+	URL                 string `json:"url" validate:"nonzero"`
+	ExpirationInMinutes int64  `json:"expiration_in_minutes" validate:"min=0"`
 }
 
 func (a *App) Initialize() {
@@ -39,14 +39,36 @@ func (a *App) InitializeRoutes() {
 func (a *App) createShortLink(w http.ResponseWriter, r *http.Request) {
 	var req shortenReq
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		respondWithError(w, StatusError{http.StatusBadRequest,
+			fmt.Errorf("parse parameter failed %v", r.Body)})
 		return
 	}
 	defer r.Body.Close()
 	if err := validator.Validate(req); err != nil {
+		respondWithError(w, StatusError{http.StatusBadRequest,
+			fmt.Errorf("validate parameter failed %v", r.Body)})
 		return
 	}
 
 	fmt.Printf("%v\n", req)
+}
+
+func respondWithError(w http.ResponseWriter, err error) {
+	switch e := err.(type) {
+	case Error:
+		log.Printf("HTTP %d - %s", e.Status(),e)
+		respondWithJSON(w, e.Status(),e.Error())
+	default:
+		respondWithJSON(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	}
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	resp,_:= json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(resp)
 }
 
 func (a *App) getShortLinkInfo(w http.ResponseWriter, r *http.Request) {
